4d04556a32674e08db660749cf587c1c94fc2dd1
Aajinkya Naik <106613699+UsernameIsNotAvailableBruh@users.noreply.github.com>
2024-07-21 03:45:19 +0000

    Added generator function and replaced second list with variable
    - Created the Prime() generator function
    - Replaced the NotPrimeBoolList list with NotPrime variable
    - Added some comments
    - Changed the print statements a little

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 import time
 
-PrimesAmount = 30 #number of prime numbers to generate
+PrimesAmount = 500 #number of prime numbers to generate
 
 start = time.perf_counter()
 
 PrimesList = [2]
 count = 1
+NotPrime = False
 while len(PrimesList) != PrimesAmount:
-  count += 2 #after 2, all primes are odd so we can skip even numbers
-  NotPrimeBoolList = [False]*len(PrimesList)
-  for x in PrimesList[1:]:
+  count += 2 #after the prime number 2, all primes are odd so we can skip even numbers
+  for x in PrimesList[1:]:#[1:] to skip first number (2)
     if count % x == 0:
-      NotPrimeBoolList[PrimesList.index(x)] = True
-  if any(NotPrimeBoolList):
+      NotPrime = True
+      break
+  if NotPrime:
+    NotPrime = False
     continue
   PrimesList.append(count)
 
 end = time.perf_counter()
-
-print(end-start)
+print(f"Found {len(PrimesList)} primes in {end-start} seconds")
 print(PrimesList)
+
+#generator that generates primes
+def Primes():
+  PrimesList = [2]
+  count = 1
+  yield 2
+  NotPrime = False
+  while True:
+    count += 2
+    for x in PrimesList[1:]:
+      if count % x == 0:
+        NotPrime = True
+        break
+    if NotPrime:
+      NotPrime = False
+      continue
+    yield count
+    PrimesList.append(count)
